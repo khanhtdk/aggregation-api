502f9dbefa00bb3c62a1776fcf087d4f486936c5
Khanh Tran <khanhtd@gmail.com>
2025-05-17 06:49:17 +0700

    Create abstract BaseQuery for controllers leveraging SQLite connection

--- app/controllers.py
+++ app/controllers.py
@@ -1,19 +1,53 @@
-from sqlalchemy import text
-from .models import db
+from abc import ABC, abstractmethod
+from typing import List
+
+from . import app
+from .utils import SQLite
 
 
-def query_monthly_sales():
-    query = '''
-        SELECT
-            STRFTIME('%Y', date_purchased) AS year,
-            STRFTIME('%m', date_purchased) AS month,
-            SUM(revenue) AS total_revenue
-        FROM orders
-        GROUP BY year, month
-        ORDER BY year, month
-    '''
-    cursor = db.session.execute(text(query))
-    columns = list(cursor.keys())
-    return list(map(lambda i: dict(zip(columns, i)), cursor.all()))
+class BaseQuery(ABC):
+    def __init__(self, profile: int = 0):
+        """
+        :param profile:     Which profile is selected. Default is the first
+                            profile starting from 0.
+        """
+        self.profile: int = profile
+        self.db = SQLite(app.config['DATABASE_FILE'])
+
+    @abstractmethod
+    def available_profiles(self) -> List[str]:
+        """
+        Returns a list of query statements. Each query statement serves a
+        performance profile and can be retrieved by its index.
+        """
+
+    def parse_results(self, results):
+        return results
+
+    @property
+    def query(self) -> str:
+        return self.available_profiles()[self.profile]
+
+    def __call__(self):
+        with self.db as conn:
+            return self.parse_results(conn.fetchall(self.query))
 
 
+class MonthlySalesQuery(BaseQuery):
+    def parse_results(self, results):
+        parse = lambda i: {'year': i[0], 'month': i[1], 'revenue': i[2]}
+        return list(map(parse, results))
+
+    def available_profiles(self) -> List[str]:
+        return [
+            # Query that uses the original Order model and leverages STRFTIME
+            '''
+                SELECT
+                    STRFTIME('%Y', date_purchased) AS year,
+                    STRFTIME('%m', date_purchased) AS month,
+                    SUM(revenue)
+                FROM orders
+                GROUP BY year, month
+                ORDER BY year, month
+            ''',
+        ]
