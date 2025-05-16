70dfeb8d78b87c7cd921aaf14fc8d1c6e15fd9d3
Khanh Tran <khanhtd@gmail.com>
2025-05-17 06:52:12 +0700

    New SplitDateOrder model: an Order clone with split date fields

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from flask_sqlalchemy import SQLAlchemy
+
 from . import app
 
 db = SQLAlchemy(app)
@@ -26,3 +29,22 @@ class Order(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
     revenue = db.Column(db.Numeric(10, 2, asdecimal=False), nullable=False)
     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
+
+
+class SplitDateOrder(db.Model):
+    __tablename__ = 'split_date_orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Split `date_purchased` into 3 separate fields
+    year = db.Column(db.String(4), nullable=False)
+    month = db.Column(db.String(2), nullable=False)
+    day = db.Column(db.String(2), nullable=False)
+
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    revenue = db.Column(db.Numeric(10, 2, asdecimal=False), nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
+
+    @property
+    def date_purchased(self):
+        return datetime.date(int(self.year), int(self.month), int(self.day))
