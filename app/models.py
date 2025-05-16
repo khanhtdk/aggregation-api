5d6b6142cd908a4778e766cc03f648a4e12ce67a
Khanh Tran <khanhtd@gmail.com>
2025-05-16 10:24:51 +0700

    Implement essential models

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from . import db
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+
+
+class Region(db.Model):
+    __tablename__ = 'regions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_purchased = db.Column(db.Date, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    revenue = db.Column(db.Numeric(10, 2, asdecimal=False), nullable=False)
+    region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
