e466eee37a1aa9bb1203e40773e5459ac08408e4
Khanh Tran <khanhtd@gmail.com>
2025-05-16 09:49:26 +0700

    Init db instance & schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+from . import models
+from . import views
+
+with app.app_context():
+    db.create_all()
