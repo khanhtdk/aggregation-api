1433dbea0dec6e63c25479dce86f8174df8274c3
Khanh Tran <khanhtd@gmail.com>
2025-05-17 09:31:10 +0700

    Refactor model query and creation with ModelUtils

--- app/models.py
+++ app/models.py
@@ -1,27 +1,45 @@
 import datetime
+from typing import Tuple
 
 from flask_sqlalchemy import SQLAlchemy
-
 from . import app
 
 db = SQLAlchemy(app)
 
 
-class Product(db.Model):
+class ModelUtils:
+    @classmethod
+    def new(cls, **kwargs) -> db.Model:
+        obj = cls(**kwargs)
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+
+    @classmethod
+    def get_or_create(cls, defaults=None, **queries) -> Tuple[db.Model, bool]:
+        obj = db.session.query(cls).filter_by(**queries).one_or_none()
+        existed = obj is not None
+        if not existed:
+            defaults = defaults or {}
+            obj = cls.new(**queries, **defaults)
+        return obj, not existed
+
+
+class Product(db.Model, ModelUtils):
     __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False, unique=True)
 
 
-class Region(db.Model):
+class Region(db.Model, ModelUtils):
     __tablename__ = 'regions'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False, unique=True)
 
 
-class Order(db.Model):
+class Order(db.Model, ModelUtils):
     __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -30,8 +48,17 @@ class Order(db.Model):
     revenue = db.Column(db.Numeric(10, 2, asdecimal=False), nullable=False)
     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
 
+    @classmethod
+    def new(cls, date_purchased: datetime.date, product: Product, revenue: float, region: Region) -> 'Order':
+        return super().new(
+            date_purchased=date_purchased,
+            product_id=product.id,
+            revenue=revenue,
+            region_id=region.id
+        )
 
-class SplitDateOrder(db.Model):
+
+class SplitDateOrder(db.Model, ModelUtils):
     __tablename__ = 'split_date_orders'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -45,6 +72,15 @@ class SplitDateOrder(db.Model):
     revenue = db.Column(db.Numeric(10, 2, asdecimal=False), nullable=False)
     region_id = db.Column(db.Integer, db.ForeignKey('regions.id'), nullable=False)
 
-    @property
-    def date_purchased(self):
-        return datetime.date(int(self.year), int(self.month), int(self.day))
+    @classmethod
+    def new(cls, date_purchased: datetime.date, product: Product, revenue: float, region: Region) -> 'Order':
+        year, month, day = date_purchased.strftime('%Y-%m-%d').split('-')
+        return super().new(
+            date_purchased=date_purchased,
+            year=year,
+            month=month,
+            day=day,
+            product_id=product.id,
+            revenue=revenue,
+            region_id=region.id
+        )
